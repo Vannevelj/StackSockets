a3df91e2abe74aeb116c02b29dc0b7dffbded668
Jeroen Vannevel <jer_vannevel@hotmail.com>
2014-05-19 04:10:03 +0200

    Showcase example of connecting to SO websockets and reading/displaying the data

---
+++ StackSockets/Library/StackSocket.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class StackSocket
+    {
+        ClientWebSocket socket = new ClientWebSocket();
+        private Uri uri;
+
+        public event EventHandler<SocketEventArgs> OnSocketReceive;
+
+        public StackSocket(string url)
+        {
+            uri = new Uri(url);
+        }
+
+        public async Task Connect()
+        {
+            if (socket.State != WebSocketState.Open && socket.State != WebSocketState.Connecting)
+            {
+                await socket.ConnectAsync(uri, CancellationToken.None);
+            }
+
+            var request = Encoding.UTF8.GetBytes("155-questions-active");
+
+            await socket.SendAsync(new ArraySegment<byte>(request), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            await Receive();
+        }
+
+        private async Task Receive()
+        {
+            var buffer = new byte[1024];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var response = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (response.MessageType == WebSocketMessageType.Close)
+                {
+                    await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Close response received", CancellationToken.None);
+                } else
+                {
+                    var result = Encoding.UTF8.GetString(buffer);
+
+                    OnSocketReceive.Invoke(this, new SocketEventArgs { Response = result });
+                }
+            }
+        }
+    }
+
+    public class SocketEventArgs : EventArgs
+    {
+        public string Response { get; set; }
+    }
+}
--- StackSockets/StackSockets/Program.cs
+++ StackSockets/StackSockets/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library;
 
 namespace StackSockets
 {
@@ -10,6 +11,16 @@ namespace StackSockets
     {
         static void Main(string[] args)
         {
+            var socket = new StackSocket("wss://qa.sockets.stackexchange.com");
+            socket.OnSocketReceive += OnDataReceived;
+            socket.Connect();
+
+            Console.ReadKey();
+        }
+
+        private static void OnDataReceived(object sender, SocketEventArgs e)
+        {
+            Console.WriteLine(e.Response);
         }
     }
 }
